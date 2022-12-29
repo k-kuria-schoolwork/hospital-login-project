f19d3336a75d58098ca3779c4fbb1c0f786202fe
sammy <118251741+samuelkuria01@users.noreply.github.com>
2022-12-29 10:28:26 +0300

    Add files via upload

---
+++ PUBLIC PROJECTS/doc.js
@@ -0,0 +1,10 @@
+let signInBtn = document.getElementById('signInBtn');
+let signUpBtn = document.getElementById('signUpBtn');
+let title = document.getElementById('title');
+let fieldname = document.getElementById('fieldname');
+
+
+signInBtn.onclick = function (){
+    fieldname.style.maxHeight = "0";
+
+}
